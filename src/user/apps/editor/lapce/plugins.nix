bf395fd0894234531c3ff62a710350d9911c9fa0
Timon Schelling <me@timon.zip>
2024-08-17 21:30:56 +0000

    imp: make symlinking lapce plugins more idiomatic

--- src/user/apps/editor/lapce/plugins.nix
+++ src/user/apps/editor/lapce/plugins.nix
@@ -1,7 +1,7 @@
 { plugins, pkgs }:
 
 let
-  fetchPluginTarZstd = { author, name, version, hash }:  pkgs.stdenvNoCC.mkDerivation (
+  fetchPluginTarball = { author, name, version, hash }:  pkgs.stdenvNoCC.mkDerivation (
     let
       url = "https://plugins.lapce.dev/api/v1/plugins/${author}/${name}/${version}/download";
       file = "lapce-plugin-${author}-${name}-${version}.tar.zstd";
@@ -13,32 +13,31 @@ let
       outputHashMode = "flat";
       outputHash = hash;
       nativeBuildInputs = [ pkgs.curl pkgs.cacert ];
-      builder = builtins.toFile "download-lapce-plugin.sh"
-      ''
+      builder = builtins.toFile "download-lapce-plugin.sh" ''
         source "$stdenv/setup"
         url="$(curl ${url})"
         curl -L "$url" -o "$out"
       '';
     }
   );
-  fetchPlugin = { author, name, version, hash } @ args: pkgs.stdenvNoCC.mkDerivation (
-    let
-      dir = "${author}-${name}-${version}";
-    in
-    {
-      name = "lapce-plugin-${dir}";
-      src = fetchPluginTarZstd args;
-      unpackPhase = "true";
-      nativeBuildInputs = [ pkgs.zstd ];
-      installPhase = ''
-        mkdir -p $out/${dir}
-        tar -C $out/${dir} -xvf $src
-      '';
-    }
-  );
-  fetchPlugins = plugins: pkgs.symlinkJoin {
-    name = "lapce-plugins";
-    paths = builtins.map fetchPlugin plugins;
+  fetchPlugin = { author, name, version, hash } @ args: pkgs.stdenvNoCC.mkDerivation {
+    name = "lapce-plugin-${author}-${name}-${version}";
+    src = fetchPluginTarball args;
+    unpackPhase = "true";
+    nativeBuildInputs = [ pkgs.zstd ];
+    installPhase = ''
+      mkdir -p $out
+      tar -C $out -xvf $src
+    '';
   };
+  fetchPlugins = plugins: pkgs.linkFarm "lapce-plugins" (builtins.listToAttrs (
+    builtins.map (
+      { author, name, version, ... } @ plugin:
+      {
+        name = "${author}-${name}-${version}";
+        value = fetchPlugin plugin;
+      }
+    ) plugins
+  ));
 in
 fetchPlugins plugins
