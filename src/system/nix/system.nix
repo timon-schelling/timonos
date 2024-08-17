4ffca827bd7e596e7e630749e1a6e013ad076b9a
Timon Schelling <me@timon.zip>
2024-08-17 19:08:31 +0000

    feat: use nh in switch and build helper scripts

--- src/system/nix/system.nix
+++ src/system/nix/system.nix
@@ -1,8 +1,9 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
   system.stateVersion = config.opts.system.stateVersion;
   nixpkgs.hostPlatform = config.opts.system.platform;
   nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  environment.systemPackages = [ pkgs.nh ];
 }
