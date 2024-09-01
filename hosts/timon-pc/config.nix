63fb9f618a0573f6bef85ee5c61ba112add2e69f
Timon Schelling <me@timon.zip>
2024-09-01 23:18:46 +0000

    feat: add steam for timon-pc

--- hosts/timon-pc/config.nix
+++ hosts/timon-pc/config.nix
@@ -41,4 +41,10 @@
 
   # disable the tpm module because is not supported and causes failures during boot
   boot.blacklistedKernelModules = [ "tpm" "tpm_atmel" "tpm_infineon" "tpm_nsc" "tpm_tis" "tpm_crb" ];
+
+  # steam
+  hardware.graphics.enable32Bit = true;
+  opts.users.timon.home.persist.state.folders = [ ".local/share/Steam" ];
+  home-manager.users.timon.home.packages = [ pkgs.steam ];
+  # for unity games set data folder with `XDG_CONFIG_HOME=~/.local/share/Steam/data %command%` in steam launch options
 }
