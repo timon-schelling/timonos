c510aaf3a8ab9be111d4c41718d95ab934f4205f
Timon Schelling <me@timon.zip>
2024-09-01 23:17:43 +0000

    feat: add tuigreet inside terminal inside cage composer as a login manager

--- src/system/login/system.nix
+++ src/system/login/system.nix
@@ -6,33 +6,59 @@ in
 {
   options = {
     opts.system.login.greeter = lib.mkOption {
-      type = lib.types.enum [ "tui" "tuigreet" "gui" "regreet" ];
-      default = "gui";
+      type = lib.types.enum [ "tui" "gui" "tty" ];
+      default = "tui";
     };
   };
 
   config = lib.mkMerge [
-    (lib.mkIf (cfg.greeter == "tui" || cfg.greeter == "tuigreet") {
-      platform.system.persist.folders = [
-        {
-          directory = "/var/cache/tuigreet";
-          user = "greeter";
-          group = "greeter";
-          mode = "0755";
-        }
-      ];
-
+    (lib.mkIf (cfg.greeter == "tui") {
       services.greetd = {
         enable = true;
-        vt = 7;
         settings.default_session = {
-          command = "${pkgs.greetd.tuigreet}/bin/tuigreet -g 'TimonOS' --time --remember --remember-user-session --asterisks";
+          command = let
+            terminalConfigFile = (pkgs.formats.toml { }).generate "greeter-terminal-config.toml" {
+              cursor = "|";
+              blinking-cursor = false;
+              padding-x = 0;
+              navigation.mode = "CollapsedTab";
+              colors = {
+                background = "#1c1c1c";
+                foreground = "#aaaaaa";
+                cursor = "#aaaaaa";
+                selection-background = "#888888";
+                selection-foreground = "#1c1c1c";
+
+                black = "#464646";
+                white = "#727272";
+
+                dim-black = "#272727";
+              };
+              fonts = {
+                size = 44;
+              };
+              window.decorations = "Disabled";
+            };
+          in
+          (pkgs.nu.writeScript "greetd-terminal-rio" ''
+            mkdir /tmp/greeter-home/.config/rio
+            cp ${terminalConfigFile} /tmp/greeter-home/.config/rio/config.toml
+            (
+              ${pkgs.cage}/bin/cage -m last --
+              ${pkgs.rio}/bin/rio --command
+              ${pkgs.greetd.tuigreet}/bin/tuigreet
+                -g "" --time --time-format "%Y-%m-%d %H:%M:%S"
+                --remember --remember-user-session --asterisks
+            )
+          '');
           user = "greeter";
         };
       };
+
+      users.users.greeter.home = "/tmp/greeter-home";
     })
 
-    (lib.mkIf (cfg.greeter == "gui" || cfg.greeter == "regreet") {
+    (lib.mkIf (cfg.greeter == "gui") {
       platform.system.persist.folders = [
         {
           directory = "/var/cache/regreet";
@@ -65,6 +91,28 @@ in
           "last"
         ];
       };
+    })
+
+    (lib.mkIf (cfg.greeter == "tty") {
+      services.greetd = {
+        enable = true;
+        vt = 7;
+        settings.default_session = {
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet -g '' --time --remember --remember-user-session --asterisks";
+          user = "greeter";
+        };
+      };
+    })
+
+    (lib.mkIf (cfg.greeter == "tui" || cfg.greeter == "tty") {
+      platform.system.persist.folders = [
+        {
+          directory = "/var/cache/tuigreet";
+          user = "greeter";
+          group = "greeter";
+          mode = "0755";
+        }
+      ];
     })
   ];
 }
