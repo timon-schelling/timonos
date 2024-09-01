e6c79c7e40f769a6dc5808d5cc552f9205017227
Timon Schelling <me@timon.zip>
2024-09-01 23:13:46 +0000

    feat: add some lv2 plugins to pipewire

--- src/system/audio/system.nix
+++ src/system/audio/system.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   hardware.pulseaudio.enable = false;
@@ -8,6 +8,11 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     jack.enable = true;
+    extraLv2Packages = [
+      pkgs.lsp-plugins
+      pkgs.bankstown-lv2
+      pkgs.rnnoise-plugin
+    ];
     wireplumber.enable = true;
   };
 }
