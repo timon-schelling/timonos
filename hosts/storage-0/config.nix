b12bfb650a242d9cf5910e2b3523b25c76656aa5
Timon Schelling <me@timon.zip>
2024-09-05 13:18:16 +0000

    feat: add host storage-0

---
+++ hosts/storage-0/config.nix
@@ -0,0 +1,17 @@
+{
+  opts = {
+    system = {
+      drive = "/dev/disk/by-id/ata-SAMSUNG_MZNLF128HCHP-00000_S28TNXAGB25148";
+      swap.size = "16G";
+    };
+    users = {
+      user = {
+        passwordHash = "$6$rounds=262144$qsdAwOt1Y50jB7oF$0oYPN0qt7pm7Dv4qD75o.gOD28THHSxNxGWRMGAdbOgQ6H.if236GYIyTVe.n502fSSCZ.MYEY0RtBCqerhvF1";
+        home = {
+          name = "User";
+          email = "user.storage-0.homelab@timon.zip";
+        };
+      };
+    };
+  };
+}
