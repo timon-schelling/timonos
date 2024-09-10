7ed1566c035a98bcec317974698ff6f0060e70ae
Timon Schelling <me@timon.zip>
2024-09-10 19:23:13 +0000

    imp: remove tere first run prompt without patching

--- overlays/tere/overlay.nix
+++ overlays/tere/overlay.nix
@@ -1,7 +1,26 @@
 inputs: self: super: {
-  tere = super.tere.overrideAttrs {
-    patches = [ ./remove-first-run-prompt.patch ];
-    doCheck = false;
-    doInstallCheck = false;
+  # TODO: remove this once the https://github.com/mgunyho/tere/pull/104 is merged and packaged in nixpkgs unstable
+  tere-unwrapped = super.tere.override {
+    rustPlatform = super.rustPlatform // {
+      buildRustPackage = args: self.rustPlatform.buildRustPackage (args // {
+        version = "576dfab4efce0048d34712e33d181a20e9dca1a7";
+        src = super.fetchFromGitHub {
+          owner = "timon-schelling";
+          repo = "tere";
+          rev = "576dfab4efce0048d34712e33d181a20e9dca1a7";
+          hash = "sha256-VvL5xqWQhfu8BSKEYZINk6tma0TXy7rQnFWft/fA6vI=";
+        };
+        cargoHash = "sha256-FrI3nk/ZeIHp9KMHFYIYzm8FiFhQFzdGoGhn445F3H0=";
+        doCheck = false;
+      });
+    };
   };
+  tere = (self.runCommand "tere-wrapped"
+    {
+      buildInputs = [ self.makeWrapper ];
+    }
+    ''
+      makeWrapper ${self.tere-unwrapped}/bin/tere $out/bin/tere --add-flags "--skip-first-run-prompt"
+    ''
+  );
 }
