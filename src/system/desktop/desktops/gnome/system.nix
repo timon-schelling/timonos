9fa7c549f094086a8f05f775f2e4d8e6f4fbb6d1
Timon Schelling <me@timon.zip>
2024-09-16 12:54:07 +0000

    feat: add gnome desktop

---
+++ src/system/desktop/desktops/gnome/system.nix
@@ -0,0 +1,13 @@
+{ lib, config, pkgs, inputs, ... }@args:
+
+let
+  cfg = config.opts.system.desktops.gnome;
+in
+{
+  options.opts.system.desktops.gnome.enable = lib.mkEnableOption "Gnome Desktop";
+  config = lib.mkIf cfg.enable {
+    services.xserver.desktopManager.gnome.enable = true;
+    services.gnome.core-utilities.enable = false;
+    environment.gnome.excludePackages = [ pkgs.gnome-tour pkgs.xdg-user-dirs ];
+  };
+}
