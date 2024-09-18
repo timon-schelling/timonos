912518eb4917d802098300208a2fdf06571dac9f
Timon Schelling <me@timon.zip>
2024-09-18 17:24:11 +0000

    feat: add host homelab-0

---
+++ hosts/homelab-0/config.nix
@@ -0,0 +1,75 @@
+{
+  opts = {
+    system = {
+      drive = "/dev/disk/by-id/ata-SAMSUNG_MZNLN256HMHQ-000H1_S2Y2NX0HC43252";
+      swap.size = "16G";
+      desktops.gnome.enable = true;
+    };
+    users = {
+      timon = {
+        passwordHash = "$6$rounds=262144$btdA4Fl2MtXbCcEw$wzDDnSCaBlgUYNIXQm0fK8dKjHQAPFP6AiQz6qpZi3l9/h69WmbMAhSNtPYN5qSGcEw4yJGQT4W0KdPFvAcYg0";
+        admin = true;
+        home = {
+          name = "Timon Schelling";
+          email = "me@timon.zip";
+          profiles.default-desktop-apps.enable = true;
+          persist.data.folders = [
+            "tmp"
+            "data"
+          ];
+          desktops.gnome.mac.enable = true;
+          apps = {
+            terminal = {
+              rio.enable = true;
+            };
+            settings = {
+              wifi.enable = true;
+              audio.enable = true;
+            };
+            editor = {
+              vscode.enable = true;
+            };
+            browser = {
+              firefox.enable = true;
+              chromium.enable = true;
+              epiphany.enable = true;
+            };
+            filemanager = {
+              nautilus.enable = true;
+            };
+            other = {
+              spotify.enable = true;
+            };
+          };
+        };
+      };
+    };
+  };
+
+  # TODO: find a better way
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = true;
+  };
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  boot = {
+    kernelModules = [ "kvm-intel" ];
+    initrd = {
+      kernelModules = [ "amdgpu" ];
+      availableKernelModules = [ "xhci_pci" "ahci" "ehci_pci" "usb_storage" "sd_mod" "sr_mod" "sdhci_pci" ];
+    };
+  };
+
+  hardware = {
+    enableRedistributableFirmware = true;
+    cpu.amd.updateMicrocode = true;
+  };
+
+  # disable the tpm module because it not supported and causes failures during boot
+  boot.blacklistedKernelModules = [ "tpm" "tpm_atmel" "tpm_infineon" "tpm_nsc" "tpm_tis" "tpm_crb" ];
+}
