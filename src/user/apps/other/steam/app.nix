4b451474b8cd28b9063cce84cec1e5b9fc6ef521
Timon Schelling <me@timon.zip>
2024-09-20 00:22:10 +0000

    fix: steam app persistence

--- src/user/apps/other/steam/app.nix
+++ src/user/apps/other/steam/app.nix
@@ -2,6 +2,6 @@
 
 {
   # for unity games set data folder with `XDG_CONFIG_HOME=~/.local/share/Steam/data %command%` in steam launch options
-  persist.state.folders = [ ".local/share/Steam" ];
+  platform.user.persist.folders = [ ".local/share/Steam" ];
   home.packages = [ pkgs.steam ];
 }
