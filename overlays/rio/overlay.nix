e626270b24f422f5f3559a21bc9ac31302dbeb10
Timon Schelling <me@timon.zip>
2025-01-06 15:37:35 +0000

    dep: update rio terminal

--- overlays/rio/overlay.nix
+++ overlays/rio/overlay.nix
@@ -6,8 +6,8 @@ inputs: self: super: {
         src = self.fetchFromGitHub {
           owner = "raphamorim";
           repo = "rio";
-          rev = "8df8d407765333c1067f5ce0460573b79e946da2";
-          hash = "sha256-Iz4OcrqOgXqxblLYTrW7a3lftOAMXgcWiW1+ATSkOEY=";
+          rev = "d810426a3239fdeda3296afbc85c816bde04d055";
+          hash = "sha256-hpDOyPjZKAdWBEC1kZb58dBjAnSUYrUSc4SSTeKTyow=";
         };
         cargoHash = "sha256-kDZjGDzirLu3gLNmV0FheeaCwH6GltI4URywS36nRjI=";
         doCheck = false;
