2481378aa13d5986c1d3fba7718f6e2d3d27ba13
Timon Schelling <me@timon.zip>
2025-02-28 13:32:02 +0000

    dep: update contain package

--- overlays/contain/package.nix
+++ overlays/contain/package.nix
@@ -10,8 +10,8 @@ rustPlatform.buildRustPackage {
   src = fetchFromGitHub {
     owner = "timon-schelling";
     repo = "contain";
-    rev = "44fa657b48c8aeb4472095d6d5d16a13cce34038";
-    hash = "sha256-lKglyaRNqhC71wfhZje4ARP6dPvmSjIKYeoWYcwsK1Y=";
+    rev = "7c0615bb297eef0dad8b44fcadfc45119478abbd";
+    hash = "sha256-YRDaGh2zLUt+CLwCKnFdZrinb7C39O2O6p2nK7j/2Ms=";
   };
 
   cargoHash = "sha256-37KDDjcIl0MN91IFJ8TRS4VPvAemdMFuQb+q5Fyxeno=";
