d619545acc2a3a79a8a84b29a65c98afa8d8783f
Timon Schelling <me@timon.zip>
2025-03-07 01:32:04 +0000

    imp: hide error when atuin search command is not available

--- src/user/shells/nu/home.nix
+++ src/user/shells/nu/home.nix
@@ -74,7 +74,7 @@
           modifier: shift
           keycode: up
           mode: [emacs, vi_normal, vi_insert]
-          event: { send: executehostcommand cmd: (do { _atuin_search_cmd }) }
+          event: { send: executehostcommand cmd: (do { try { _atuin_search_cmd } }) }
         }
       )
     )
