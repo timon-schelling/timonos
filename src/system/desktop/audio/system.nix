b6ee9a64e9e0b5f825675339448191fae9e12eb6
Timon Schelling <me@timon.zip>
2025-03-13 01:20:57 +0000

    fix: switch to using renamed options

--- src/system/desktop/audio/system.nix
+++ src/system/desktop/audio/system.nix
@@ -1,18 +1,20 @@
 { pkgs, ... }:
 
 {
-  hardware.pulseaudio.enable = false;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-    extraLv2Packages = [
-      pkgs.lsp-plugins
-      pkgs.bankstown-lv2
-      pkgs.rnnoise-plugin
-    ];
-    wireplumber.enable = true;
+  services = {
+    pulseaudio.enable = false;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+      extraLv2Packages = [
+        pkgs.lsp-plugins
+        pkgs.bankstown-lv2
+        pkgs.rnnoise-plugin
+      ];
+      wireplumber.enable = true;
+    };
   };
 }
--- src/user/apps/editor/vscode/app.nix
+++ src/user/apps/editor/vscode/app.nix
@@ -20,50 +20,52 @@
         cp "${pkgs.vscode}/share/applications/code.desktop" "$out/share/applications/"
       ''
     );
-    extensions = with pkgs.vscode-extensions; [
-      jnoortheen.nix-ide
-      mhutchie.git-graph
-      thenuprojectcontributors.vscode-nushell-lang
-      ms-vscode.hexeditor
-      streetsidesoftware.code-spell-checker
-      pkief.material-icon-theme
-    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      {
-        publisher = "github";
-        name = "copilot";
-        version = "1.236.1144";
-        sha256 = "sha256-qZMRbskvPaLOCbA+tm3HZJwYALvT1u7TsZmAnK7Mzsk=";
-      }
-      {
-        publisher = "github";
-        name = "copilot-chat";
-        version = "0.22.4";
-        sha256 = "sha256-9hzcGC4LsujXG/TpvlauntYDlUGpsrUYHmiGvADv4ks=";
-      }
-      {
-        publisher = "streetsidesoftware";
-        name = "code-spell-checker-german";
-        version = "2.3.2";
-        sha256 = "sha256-40Oc6ycNog9cxG4G5gCps2ADrM/wLuKWFrD4lnd91Z4=";
-      }
-      {
-        publisher = "bierner";
-        name = "color-info";
-        version = "0.7.2";
-        sha256 = "sha256-Bf0thdt4yxH7OsRhIXeqvaxD1tbHTrUc4QJcju7Hv90=";
-      }
-      {
-        publisher = "miguelsolorio";
-        name = "min-theme";
-        version = "1.5.0";
-        sha256 = "sha256-DF/9OlWmjmnZNRBs2hk0qEWN38RcgacdVl9e75N8ZMY=";
-      }
-    ];
-    enableUpdateCheck = false;
-    enableExtensionUpdateCheck = false;
     mutableExtensionsDir = false;
-    userSettings = (builtins.fromJSON (builtins.readFile ./settings.json));
-    keybindings = (builtins.fromJSON (builtins.readFile ./keybindings.json));
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        jnoortheen.nix-ide
+        mhutchie.git-graph
+        thenuprojectcontributors.vscode-nushell-lang
+        ms-vscode.hexeditor
+        streetsidesoftware.code-spell-checker
+        pkief.material-icon-theme
+      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          publisher = "github";
+          name = "copilot";
+          version = "1.236.1144";
+          sha256 = "sha256-qZMRbskvPaLOCbA+tm3HZJwYALvT1u7TsZmAnK7Mzsk=";
+        }
+        {
+          publisher = "github";
+          name = "copilot-chat";
+          version = "0.22.4";
+          sha256 = "sha256-9hzcGC4LsujXG/TpvlauntYDlUGpsrUYHmiGvADv4ks=";
+        }
+        {
+          publisher = "streetsidesoftware";
+          name = "code-spell-checker-german";
+          version = "2.3.2";
+          sha256 = "sha256-40Oc6ycNog9cxG4G5gCps2ADrM/wLuKWFrD4lnd91Z4=";
+        }
+        {
+          publisher = "bierner";
+          name = "color-info";
+          version = "0.7.2";
+          sha256 = "sha256-Bf0thdt4yxH7OsRhIXeqvaxD1tbHTrUc4QJcju7Hv90=";
+        }
+        {
+          publisher = "miguelsolorio";
+          name = "min-theme";
+          version = "1.5.0";
+          sha256 = "sha256-DF/9OlWmjmnZNRBs2hk0qEWN38RcgacdVl9e75N8ZMY=";
+        }
+      ];
+      enableUpdateCheck = false;
+      enableExtensionUpdateCheck = false;
+      userSettings = (builtins.fromJSON (builtins.readFile ./settings.json));
+      keybindings = (builtins.fromJSON (builtins.readFile ./keybindings.json));
+    };
   };
 
   home.packages = [
