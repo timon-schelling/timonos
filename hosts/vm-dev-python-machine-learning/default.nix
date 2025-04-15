6bca86654464922c85ed4da1c5fc91fe84581bb9
Timon Schelling <me@timon.zip>
2025-04-15 08:28:56 +0000

    feat: add vm-dev-python-machine-learning

---
+++ hosts/vm-dev-python-machine-learning/default.nix
@@ -0,0 +1,26 @@
+{ lib, pkgs, config, ... }:
+
+{
+  imports = [
+    ../vm-base
+    ../vm-base-workspace
+  ];
+
+  config = {
+    environment.systemPackages = with pkgs; [
+      (python3.withPackages (p: with p; [
+        numpy
+        matplotlib
+        pyqt6
+        torch
+      ]))
+      kdePackages.qtwayland
+    ];
+
+    environment.sessionVariables.MPLBACKEND = "qtagg";
+
+    home-manager.users.timon.programs.vscode.profiles.default.extensions = with pkgs.vscode-extensions; [
+      ms-python.python
+    ];
+  };
+}
