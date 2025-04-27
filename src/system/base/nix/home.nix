d399b44b4ac4b8528827092cd1f786cbcc3ce5d4
Timon Schelling <me@timon.zip>
2025-04-27 13:45:31 +0000

    feat: nix persist eval cache

--- src/system/base/nix/home.nix
+++ src/system/base/nix/home.nix
@@ -1,6 +1,10 @@
 { config, ... }:
 
 {
+  platform.user.persist.folders = [
+    ".cache/nix"
+  ];
+
   home.stateVersion = config.opts.system.stateVersion;
   xdg.configFile."nixpkgs/config.nix".text = ''
     {
