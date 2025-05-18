eb6bb00f170c36e489a58647aefa3c834d83a947
Timon Schelling <me@timon.zip>
2025-05-18 15:50:10 +0000

    chore: remove outdated qtcreator overlay

--- overlays/qtcreator/overlay.nix
+++
@@ -1,8 +0,0 @@
-# TODO: remove when qtcreator from nixpkgs includes gtk3 gsettings-schemas dependency
-inputs: self: super: {
-  qtcreator = (self.qt6Packages.callPackage ./package.nix {
-    inherit (self.linuxPackages) perf;
-    llvmPackages = self.llvmPackages_18;
-    stdenv = self.llvmPackages_18.stdenv;
-  });
-}
