417b6faf2562584a916dfb5a756707a0726b5c8e
Timon Schelling <me@timon.zip>
2025-05-22 15:13:12 +0000

    feat: add functions for turning derivations into nu environments

--- overlays/nushell/overlay.nix
+++ overlays/nushell/overlay.nix
@@ -1,15 +1,104 @@
 inputs: self: super:
 let
-  writeNu = name: script: f: f "${name}" ''
-    #!${self.nushell}/bin/nu --stdin
+  writeNu = name: script: f:
+    f "${name}" ''
+      #!${self.nushell}/bin/nu --stdin
 
-    ${script}
-  '';
+      ${script}
+    '';
+  writeScript = name: script: writeNu name script self.writeScript;
+  writeScriptBin = name: script: writeNu name script self.writeScriptBin;
+
+  envJsonFromDrv = drv:
+    let
+      scrubbed = (builtins.removeAttrs drv.drvAttrs [
+        "allowedReferences"
+        "allowedRequisites"
+        "disallowedReferences"
+        "disallowedRequisites"
+      ]);
+      outputs' = builtins.map (n: {
+        name = n;
+        value = builtins.placeholder n;
+      }) (scrubbed.outputs or [ "out" ]);
+      outputs = builtins.listToAttrs outputs';
+      json = derivation (scrubbed // { args = [ ./get-env.sh ]; } // outputs);
+    in
+    (self.runCommand "${drv.name}-env.json" {} ''
+      ln -s ${json} $out
+    '');
+  envFromDrv = drv:
+    {
+      name ? drv.name,
+      pathLike ? [ ],
+      pathLikeDefaults ? true,
+      ignore ? [ ],
+      ignoreDefaults ? true,
+      preEnvLoadCommands ? "",
+      postEnvLoadCommands ? "",
+    }:
+    let
+      env = envJsonFromDrv drv;
+      pathLike' = pathLike ++ (if pathLikeDefaults then [
+        "PATH"
+        "LD_LIBRARY_PATH"
+        "XDG_DATA_DIRS"
+        "XDG_CONFIG_DIRS"
+      ] else []);
+      ignore' = ignore ++ (if ignoreDefaults then [
+        "HOME"
+        "USER"
+        "SHELL"
+        "TERM"
+      ] else []);
+    in
+    (self.writeScript name ''
+      let path_like_conversion = {
+          from_string: { |s| $s | split row (char esep) | path expand -n }
+          to_string: { |v| $v | path expand -n | str join (char esep) }
+      }
+
+      $env.ENV_CONVERSIONS = $env.ENV_CONVERSIONS | merge {
+      ${self.lib.concatStrings (self.lib.lists.map (x: ''
+        ${x}: $path_like_conversion,
+      '') pathLike')}
+      }
+
+      let variables = open ${env}
+          | get variables
+          | transpose key val
+          | update val { if "value" in $in { $in.value } else { "" }}
+          | each { { $in.key : $in.val } } | into record
+
+      mut variables = $variables
+
+      ${self.lib.concatStrings (self.lib.lists.map (x: ''
+        try { $variables = $variables | reject ${x} }
+      '') ignore')}
+
+      ${self.lib.concatStrings (self.lib.lists.map (x: ''
+        try { $variables = $variables | update ${x} { $in | split row ":" | append $env.${x} } }
+      '') pathLike')}
+
+      ${preEnvLoadCommands}
+
+      load-env $variables
+
+      ${postEnvLoadCommands}
+    '');
+    shellBinFromDrv = drv: args:
+      (let
+        env = envFromDrv drv args;
+      in
+      writeScriptBin env.name ''
+        exec ${self.nushell}/bin/nu -e "source ${env}"
+      '');
+
   nu = {
-    writeScript = name: script: writeNu name script self.writeScript;
-    writeScriptBin = name: script: writeNu name script self.writeScriptBin;
+    inherit writeScript;
+    inherit writeScriptBin;
+
+    inherit envFromDrv;
+    inherit shellBinFromDrv;
   };
-in
-{
-  inherit nu;
-}
+in { inherit nu; }
