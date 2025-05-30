c5a4ed9eaa675358d587f291bd40c20878b29310
Timon Schelling <me@timon.zip>
2025-05-30 15:02:43 +0000

    dep: update zen-browser

--- overlays/zen-browser/overlay.nix
+++ overlays/zen-browser/overlay.nix
@@ -1,4 +1,4 @@
 inputs: self: super: rec {
   zen-browser-unwrapped = super.callPackage ./package.nix { };
-  zen-browser = super.wrapFirefox zen-browser-unwrapped { };
+  zen-browser = super.wrapFirefox zen-browser-unwrapped { pname = "zen-browser"; };
 }
--- overlays/zen-browser/package.nix
+++ overlays/zen-browser/package.nix
@@ -1,80 +1,89 @@
 {
-  lib,
   stdenv,
-  makeWrapper,
-  wrapGAppsHook,
-  copyDesktopItems,
-  libGL,
-  fontconfig,
-  libxkbcommon,
-  zlib,
-  freetype,
+  config,
+  wrapGAppsHook3,
+  autoPatchelfHook,
+  patchelfUnstable,
+  adwaita-icon-theme,
+  dbus-glib,
+  libXtst,
+  curl,
   gtk3,
-  libxml2,
-  dbus,
-  xcb-util-cursor,
   alsa-lib,
-  pango,
-  atk,
-  cairo,
-  gdk-pixbuf,
-  glib,
-  xorg,
+  libva,
+  pciutils,
+  pipewire,
+  writeText,
+  fetchurl,
+  ...
 }:
-
 let
-  version = "1.7.2b";
-  hash = "1j15cmwyy0pw339c6as7afw3x4478h0cmk1xwf3zbaz63kmfbf42";
+  policies = {
+    DisableAppUpdate = true;
+  } // config.zen.policies or { };
 
-  src = builtins.fetchTarball {
-    url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.bz2";
-    sha256 = "sha256:${hash}";
-  };
-  runtimeLibs = [
-        libGL stdenv.cc.cc fontconfig libxkbcommon zlib freetype
-        gtk3 libxml2 dbus xcb-util-cursor alsa-lib pango atk cairo gdk-pixbuf glib
-  ] ++ (with xorg; [
-    libxcb libX11 libXcursor libXrandr libXi libXext libXcomposite libXdamage libXfixes libpciaccess
-  ]);
+  policiesJson = writeText "firefox-policies.json" (builtins.toJSON { inherit policies; });
 in
-stdenv.mkDerivation {
-  pname = "zen";
-  inherit version;
-  inherit src;
+stdenv.mkDerivation (finalAttrs: {
+  version = "1.12.8b";
+  pname = "zen-browser-unwrapped";
+  applicationName = "Zen Browser";
 
-  phases = [ "installPhase" "fixupPhase" ];
+  src = fetchurl {
+    url = "https://github.com/zen-browser/desktop/releases/download/${finalAttrs.version}/zen.linux-x86_64.tar.xz";
+    hash = "sha256-HOrFsHneMCb9fOGV15HWVTNiyYtEIqO/mGV+NNqjV8c=";
+  };
 
-  nativeBuildInputs = [ makeWrapper copyDesktopItems wrapGAppsHook ] ;
+  nativeBuildInputs = [
+    wrapGAppsHook3
+    autoPatchelfHook
+    patchelfUnstable
+  ];
+
+  buildInputs = [
+    gtk3
+    alsa-lib
+    adwaita-icon-theme
+    dbus-glib
+    libXtst
+  ];
+
+  runtimeDependencies = [
+    curl
+    libva.out
+    pciutils
+  ];
+
+  appendRunpaths = [
+    "${pipewire}/lib"
+  ];
 
   installPhase = ''
-    mkdir -p $out/bin && cp -r $src/* $out/bin
-    install -D $src/browser/chrome/icons/default/default128.png $out/share/icons/hicolor/128x128/apps/zen.png
+    mkdir -p "$prefix/lib/zen-${finalAttrs.version}"
+    cp -r * "$prefix/lib/zen-${finalAttrs.version}"
+
+    mkdir -p $out/bin
+    ln -s "$prefix/lib/zen-${finalAttrs.version}/zen" $out/bin/zen
+
+    mkdir -p "$out/lib/zen-${finalAttrs.version}/distribution"
+    ln -s ${policiesJson} "$out/lib/zen-${finalAttrs.version}/distribution/policies.json"
   '';
 
-  fixupPhase = ''
-    chmod 755 $out/bin/*
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/bin/zen
-    wrapProgram $out/bin/zen --set LD_LIBRARY_PATH "${lib.makeLibraryPath runtimeLibs}"
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/bin/zen-bin
-    wrapProgram $out/bin/zen-bin --set LD_LIBRARY_PATH "${lib.makeLibraryPath runtimeLibs}"
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/bin/glxtest
-    wrapProgram $out/bin/glxtest --set LD_LIBRARY_PATH "${lib.makeLibraryPath runtimeLibs}"
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/bin/updater
-    wrapProgram $out/bin/updater --set LD_LIBRARY_PATH "${lib.makeLibraryPath runtimeLibs}"
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $out/bin/vaapitest
-    wrapProgram $out/bin/vaapitest --set LD_LIBRARY_PATH "${lib.makeLibraryPath runtimeLibs}"
-  '';
-
-  passthru = {
-    inherit gtk3; # needed in the wrapper
-    libName = "zen";
-    requireSigning = false;
-    allowAddonSideload = true;
-  };
+  patchelfFlags = [ "--no-clobber-old-sections" ];
 
   meta = {
-    description = " Zen Browser, Your browser, your way!";
     mainProgram = "zen";
-    homepage = "https://www.zen-browser.app/";
+    description = ''
+      Zen is a privacy-focused browser that blocks trackers, ads, and other unwanted content while offering the best browsing experience!
+    '';
   };
-}
+
+  passthru = {
+    inherit gtk3;
+
+    libName = "zen-${finalAttrs.version}";
+    binaryName = finalAttrs.meta.mainProgram;
+    gssSupport = true;
+    ffmpegSupport = true;
+  };
+})
