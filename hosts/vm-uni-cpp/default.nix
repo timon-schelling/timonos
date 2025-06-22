5b90ac6ae55e5b263d1ce08306bbc7c3fce2390a
Timon Schelling <me@timon.zip>
2025-06-22 14:50:44 +0000

    fix: make qtcreator work inside vm-uni-cpp

--- hosts/vm-uni-cpp/default.nix
+++ hosts/vm-uni-cpp/default.nix
@@ -8,14 +8,25 @@
     ../vm-base-persist
   ];
 
-  config = {
+  config =
+    let
+      libs = [
+        pkgs.qt6.qtbase.dev
+        pkgs.qt6.full
+        pkgs.qt6.full
+        pkgs.qt6.qtbase
+        pkgs.qt6.qtwayland
+        pkgs.qt6.qt5compat
+        pkgs.gcc
+        pkgs.qt6.wrapQtAppsHook
+      ];
+      includePath = "${pkgs.stdenv.cc.cc.lib}/include/:${pkgs.lib.makeIncludePath libs}:$NIX_INCLUDE_PATH";
+    in
+    {
     environment.systemPackages = [
-      pkgs.gcc
       pkgs.lldb
       pkgs.gdb
-
-      pkgs.qt6.qtwayland
-      pkgs.qt6.qt5compat
+      pkgs.gcc
 
       pkgs.qtcreator
 
@@ -25,17 +36,18 @@
       pkgs.cmake
 
       pkgs.eclipses.eclipse-cpp
-    ];
+    ] ++ libs;
     home-manager.users.user.programs.nushell.extraConfig = lib.mkAfter ''
       source ${pkgs.nu.envFromDrv (pkgs.mkShell {
         buildInputs = [
           pkgs.cmake
-          pkgs.qt6.qtbase
-          pkgs.qt6.qtwayland
-          pkgs.qt6.qt5compat
           pkgs.qtcreator
-        ];
+        ] ++ libs;
       }) { }}
+      $env.INCLUDE_PATH = "${includePath}"
+      $env.C_INCLUDE_PATH = "${includePath}"
+      $env.CPLUS_INCLUDE_PATH = "${includePath}"
+      $env.LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib/:${pkgs.lib.makeLibraryPath libs}:$NIX_LD_LIBRARY_PATH"
     '';
   };
 }
