3ad49467578e4b1fc1d21e2b89fe4ac379da576f
Timon <me@timon.zip>
2026-03-18 12:59:00 +0000

    chore: use new names for resolved settings

--- src/system/common/networking/system.nix
+++ src/system/common/networking/system.nix
@@ -15,10 +15,14 @@
 
   services.resolved = {
     enable = true;
-    dnssec = "true";
-    domains = [ "~." ];
-    fallbackDns = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
-    dnsovertls = "true";
+    settings = {
+      Resolve = {
+        DNSSEC = true;
+        Domains = [ "~." ];
+        FallbackDNS = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+        DNSOverTLS = "true";
+      };
+    };
   };
 
   systemd.network = {
