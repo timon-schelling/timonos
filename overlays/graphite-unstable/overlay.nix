a341a5fd84b8e441ef2b4c30e03f2e5446ce980a
Timon <me@timon.zip>
2026-03-31 17:24:23 +0000

    feat: auto updating graphite-unstable

--- overlays/graphite-unstable/overlay.nix
+++ overlays/graphite-unstable/overlay.nix
@@ -1,14 +1,35 @@
 inputs: self: super:
 
 let
-  flake = builtins.getFlake "github:GraphiteEditor/Graphite/a10092c10ce28a86819e7d39f5dc2bd02725d058";
+  # flake = builtins.getFlake "github:GraphiteEditor/Graphite/a10092c10ce28a86819e7d39f5dc2bd02725d058";
+  graphite-unstable-script = self.nu.writeScriptBin "graphite-unstable" ''
+    def main [...args] {
+      let cutoff = (date now) - 1.5hr
+
+      let sha = (
+        http get "https://api.github.com/repos/GraphiteEditor/Graphite/commits?sha=master&per_page=10"
+          --headers {Accept: "application/vnd.github+json"}
+        | where { |commit| $commit.commit.author.date | into datetime | $in < $cutoff }
+        | first
+        | get sha
+      )
+
+      let pkg = $"github:GraphiteEditor/Graphite/($sha)"
+
+      ${self.ghostty}/bin/ghostty -e ${self.bash}/bin/sh -c $"${self.nix}/bin/nix --extra-experimental-features 'nix-command flakes' build '($pkg)' || { echo 'Build failed, press enter to close'; read _; }"
+
+      ${self.nix}/bin/nix --extra-experimental-features 'nix-command flakes' run $pkg -- ...$args
+    }
+  '';
 in
 {
   graphite-unstable = self.runCommand "graphite-unstable" {} ''
     mkdir -p $out
-    cp -r ${flake.packages.${super.stdenv.hostPlatform.system}.default}/* $out/
+    cp -r ${graphite-unstable-script}/* $out/
     chmod -R u+w $out
-    mv $out/bin/graphite $out/bin/graphite-unstable
+    mkdir -p $out/share
+    cp ${super.graphite}/share/* $out/share/ -r
+    chmod -R u+w $out
     mv $out/share/applications/art.graphite.Graphite.desktop $out/share/applications/art.graphite.GraphiteUnstable.desktop
     sed -i 's/Exec=.*/Exec=graphite-unstable/' $out/share/applications/art.graphite.GraphiteUnstable.desktop
     sed -i 's/Name=.*/Name=Graphite Unstable/' $out/share/applications/art.graphite.GraphiteUnstable.desktop
