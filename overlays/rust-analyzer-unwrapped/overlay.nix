a6be8f03eeb8cec2c388fd177bf29eae1ee78731
Timon <me@timon.zip>
2026-03-31 19:01:57 +0000

    fix: apply rust-analyzer patch see https://github.com/rust-lang/rust-analyzer/issues/21855

--- overlays/rust-analyzer-unwrapped/overlay.nix
+++ overlays/rust-analyzer-unwrapped/overlay.nix
@@ -1,3 +1,10 @@
 inputs: self: super: {
-  rust-analyzer-unwrapped = (self.callPackage ./package.nix {});
+  rust-analyzer-unwrapped = super.rust-analyzer-unwrapped.overrideAttrs (old: {
+    patches = old.patches or [] ++ [
+      (self.fetchpatch {
+        url = "https://patch-diff.githubusercontent.com/raw/rust-lang/rust-analyzer/pull/21915.patch";
+        hash = "sha256-/t4KAgbOAI1fhHKLmp+f6wenozB/cjYxZZSDIFY/zkc=";
+      })
+    ];
+  });
 }
