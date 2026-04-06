5d7c3c4817ed50b3ba4c0055be714e8595207366
Timon <me@timon.zip>
2026-04-06 16:06:16 +0000

    chore: add graphite-dev cache

--- src/system/base/nix/system.nix
+++ src/system/base/nix/system.nix
@@ -4,10 +4,12 @@
   nix.settings.substituters = [
     "https://nix-community.cachix.org"
     "https://graphite.cachix.org"
+    "https://graphite-dev.cachix.org"
   ];
   nix.settings.trusted-public-keys = [
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     "graphite.cachix.org-1:B7Il1yMpkquN/dXM+5GRmz+4Xmu2aaCS1GcWNfFhsOo="
+    "graphite-dev.cachix.org-1:RppXYpiV1qO2TYKTkXXGHsAEQDOB5G51b3VlrN9QmbI="
   ];
   nix.registry = {
     pkgs.flake = inputs.self;
