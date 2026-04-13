8a0ee99817f1412e6a20523bde9e4fe9ca203267
Timon <me@timon.zip>
2026-04-13 11:11:22 +0000

    dep: update flake

--- src/user/desktops/gnome/mac/gnome-shell/desktop-home.nix
+++ src/user/desktops/gnome/mac/gnome-shell/desktop-home.nix
@@ -43,6 +43,10 @@ in
     gtk4.extraConfig = {
       gtk-application-prefer-dark-theme = 1;
     };
+    gtk4.theme = {
+      package = themePkg;
+      name = themeName;
+    };
   };
   dconf = {
     enable = true;
