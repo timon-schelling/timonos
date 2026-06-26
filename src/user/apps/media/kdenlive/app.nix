860b20fe306f897e4b75d3ca299e76851affe22d
Timon <me@timon.zip>
2026-06-26 13:17:25 +0000

    chore: remove kdenlive workaround

--- src/user/apps/media/kdenlive/app.nix
+++ src/user/apps/media/kdenlive/app.nix
@@ -7,19 +7,6 @@
   ];
 
   home.packages = [
-    (pkgs.runCommand "kdenlive"
-      {
-        buildInputs = [ pkgs.makeWrapper ];
-        version = pkgs.kdePackages.kdenlive.version;
-        pname = pkgs.kdePackages.kdenlive.pname;
-      }
-      ''
-        makeWrapper ${lib.getExe pkgs.pkgs.kdePackages.kdenlive} $out/bin/kdenlive \
-          --prefix PATH : ${pkgs.python3}/bin \
-          --prefix FREI0R_PATH : ${pkgs.frei0r}/lib/frei0r-1
-        mkdir -p "$out/share/applications/"
-        cp -r "${pkgs.kdePackages.kdenlive}/share" "$out/share"
-      ''
-    )
+    pkgs.pkgs.kdePackages.kdenlive
   ];
 }
