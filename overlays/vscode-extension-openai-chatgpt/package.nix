b2d07a60f27216ffe1fbeb36fe595558ed81d309
Timon <me@timon.zip>
2026-07-27 21:53:13 +0000

    dep: update vscode-extension-openai-chatgpt

--- overlays/vscode-extension-openai-chatgpt/package.nix
+++ overlays/vscode-extension-openai-chatgpt/package.nix
@@ -7,7 +7,7 @@ vscode-utils.buildVscodeMarketplaceExtension {
   mktplcRef = {
     name = "chatgpt";
     publisher = "openai";
-    version = "26.5422.71525";
-    hash = "sha256-9Z2rx9RISjpwUnD4Zk6RUyBPkyf8MNEho8xA2Iv1E5s=";
+    version = "26.5721.30844";
+    hash = "sha256-daZQ7nbGRhcCaHl3rtIEVrLSU+LKK3TpG9wK4twFkj0=";
   };
 }
