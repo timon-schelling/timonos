7f18a77bf6fc54c2f467c2594cd3d619ebdfb177
Timon <me@timon.zip>
2026-08-02 00:29:31 +0000

    dep: update flake

--- src/user/apps/passwordmanager/bitwarden/app.nix
+++ src/user/apps/passwordmanager/bitwarden/app.nix
@@ -6,7 +6,6 @@
   ];
 
   home.packages = [
-    # TODO: Change back once https://github.com/NixOS/nixpkgs/issues/526914 is resolved
-    (pkgs.bitwarden-desktop.override { electron_39 = pkgs.electron_39-bin; })
+    pkgs.bitwarden-desktop
   ];
 }
